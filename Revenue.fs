6342c5ad719382f63e80d277e30fbc43a910f723
TechNik <aniket.97.mane@gmail.com>
2023-12-13 01:50:23 +0530

    Added Revenue.fs file, for visualising data using Bar chart

---
+++ Revenue.fs
@@ -0,0 +1,30 @@
+module Revenue
+
+open FSharp.Data
+open XPlot.Plotly
+
+open ReadData
+
+let barData (rawCSV: RawCSV) = 
+    // Selecting top 20 games
+    let games = rawCSV.Rows
+                                            |> Seq.toList
+                                            |> List.take 20
+
+    Bar(
+        x = [for game in games -> game.Game],
+        y = [for game in games -> game.Revenue]
+    )
+
+
+let createBar (data: Bar) =
+    data
+    |> Chart.Plot
+    |> Chart.WithOptions
+        (Options(title = "Game Revenues"))
+    |> Chart.WithXTitle("Games")
+    |> Chart.WithYTitle("Revenue (Billion $)")
+    |> Chart.WithHeight 1200
+    |> Chart.WithWidth 3000 
+    |> Chart.Show
+
