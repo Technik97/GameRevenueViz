59df9602ac947d1ad660550db838ba55220ff9ed
TechNik <aniket.97.mane@gmail.com>
2023-12-13 16:27:54 +0530

    Main file

--- Program.fs
+++ Program.fs
@@ -1,11 +1,17 @@
-﻿open Revenue
-open ReadData
+﻿open ReadData
+open Revenue
+open Games
 
 [<EntryPoint>]
 let main argv =
     loadCSVFile ()
     |> Option.map barData
     |> Option.map createBar
+    |> ignore
+
+    loadCSVFile ()
+    |> Option.map pieData
+    |> Option.map createPie
     |> ignore
 
     0
