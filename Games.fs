74d11a8359bf1d45c592fd078ef98108c85f809e
TechNik <aniket.97.mane@gmail.com>
2023-12-13 16:46:49 +0530

    minor formatting fix

--- Games.fs
+++ Games.fs
@@ -9,9 +9,9 @@ let pieData (rawCSV: RawCSV) =
     let games = rawCSV.Rows |> Seq.toList
 
     let genres = games 
-                                |> List.map(fun x -> x.``Genre(s)``)
-                                |> List.distinct
-                                |> List.take 10 
+                    |> List.map(fun x -> x.``Genre(s)``)
+                    |> List.distinct
+                    |> List.take 10 
 
     let data = genres
                |> List.map(fun x -> (x, x.Length))
