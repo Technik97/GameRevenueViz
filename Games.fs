14d7efeebdd43b39e552c4a349d4812daf44152e
TechNik <aniket.97.mane@gmail.com>
2023-12-13 16:27:26 +0530

    Added Games.fs file for drawing Pie chart showing different game genres

---
+++ Games.fs
@@ -0,0 +1,30 @@
+module Games
+
+open FSharp.Data
+open XPlot.Plotly
+
+open ReadData
+
+let pieData (rawCSV: RawCSV) = 
+    let games = rawCSV.Rows |> Seq.toList
+
+    let genres = games 
+                                            |> List.map(fun x -> x.``Genre(s)``)
+                                            |> List.distinct
+                                            |> List.take 10 
+
+    let data = genres
+               |> List.map(fun x -> (x, x.Length))
+               |> List.toSeq
+
+    data |> Chart.Pie
+
+let createPie (data: PlotlyChart) = 
+    data
+    |> Chart.WithHeight 700
+    |> Chart.WithWidth 1500
+    |> Chart.WithLegend true
+    |> Chart.Show
+
+
+            
